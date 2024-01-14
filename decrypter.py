18e1f8ef617ba48c096b2f96c70614a25aa01db3
Monique Jesus <moniquecfjesus@gmail.com>
2024-01-14 12:29:37 -0300

    Update decrypter.py

--- decrypter.py
+++ decrypter.py
@@ -12,10 +12,10 @@ key = b"testeransomwares"
 aes = pyaes.AESModeOfOperationCTR(key)
 decrypt_data = aes.decrypt(file_data)
 
-## remover o arquivo criptografado
+## removendo o arquivo criptografado
 os.remove(file_name)
 
-## criar o arquivo descriptografado
+## criando o arquivo descriptografado
 new_file = "teste.txt"
 new_file = open(f'{new_file}', "wb")
 new_file.write(decrypt_data)
